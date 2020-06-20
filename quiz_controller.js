b237637674392a0d27a718da213772680daeac5e
beas-site <58391367+beas-site@users.noreply.github.com>
2020-06-20 15:56:40 +0200

    Update quiz_controller.js

--- quiz_controller.js
+++ quiz_controller.js
@@ -2,15 +2,15 @@ function Quiz(questions) {
 this.score = 0;
 this.questions = questions;
 this.questionIndex = 0;
-}
+};
 
 Quiz.prototype.getQuestionIndex = function() {
 return this.questions[this.questionIndex];
-}
+};
 
 Quiz.prototype.isEnded = function () {
 return this.questions.length === this.questionIndex;
-}
+};
 
 Quiz.prototype.guess = function(answer) {
 this.questionIndex++;
@@ -18,4 +18,4 @@ this.questionIndex++;
 if (this.getQuestionIndex().correctAnswer(answer)) {
 this.score++;
 }
-}
+};

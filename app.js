fee157457d728f3192c1dace923908af46932c07
beas-site <58391367+beas-site@users.noreply.github.com>
2020-06-20 15:12:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-
+var questions = [
+new Question("Vraag 1", ["1", "2", "3", "4"], "1"),
+new Question("Vraag 2", ["1", "2", "3", "4"], "1"),
+new Question("Vraag 3", ["1", "2", "3", "4"], "1"),
+new Question("Vraag 4", ["1", "2", "3", "4"], "1"),
+new Question("Vraag 5", ["1", "2", "3", "4"], "1")
+];

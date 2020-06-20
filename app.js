08696bae0e9e49de64333e75e6d3c761e942e491
beas-site <58391367+beas-site@users.noreply.github.com>
2020-06-20 15:15:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ new Question("Vraag 3", ["1", "2", "3", "4"], "1"),
 new Question("Vraag 4", ["1", "2", "3", "4"], "1"),
 new Question("Vraag 5", ["1", "2", "3", "4"], "1")
 ];
+
+var quiz = new Quiz(questions);

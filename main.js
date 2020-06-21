096b1fc1c58261771c25d0151d49ba82f4b8907c
beas-site <58391367+beas-site@users.noreply.github.com>
2020-06-21 10:12:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 function check() {
+  var question1 = document.quiz.question1.value;
+  var question2 = document.quiz.question2.value;
+  var question3 = document.quiz.question3.value;
+  var correct = 0;
+  
+  if(question1 == "Amsterdam") {
+    correct++;
+  }
+  if(question2 == "optie1") {
+    correct++;
+  }
+  if(question3 == "optie3") {
+    correct++;
+  }
+  
 document.getElementById("after_submit").style.visibilty = "visible";
+document.getElementById("number_correct").innerHMTL = "Je hebt " + correct + " goed.";
 }
